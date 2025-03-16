97a20467ff44ff994d5984c4070656500f79962f
Alberto Viezzi <alberto.viezzi@euris.it>
2025-03-16 23:37:22 +0100

    refactor Home component to enhance diff handling; update BuildTable method to accept diff data and improve sorting logic

---
+++ Lapo.Mud/Comparers/OperationComparer.cs
@@ -0,0 +1,21 @@
+﻿namespace Lapo.Mud.Comparers;
+
+public class CustomOperationComparer : IComparer<object>
+{
+    static readonly Dictionary<string, int> OperationOrder = new()
+    {
+        { "ADDED", 1 },
+        { "REMOVED", 2 },
+        { "UPDATED_OLD", 3 },
+        { "UPDATED_NEW", 4 }
+    };
+
+    public int Compare(object? x, object? y)
+    {
+        if (x is string xStr && y is string yStr)
+        {
+            return OperationOrder[xStr].CompareTo(OperationOrder[yStr]);
+        }
+        return 0;
+    }
+}
