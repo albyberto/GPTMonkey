095eb2a4a3003a903cf5dc36a49ad142e0d44c1a
Alberto Viezzi <alberto.viezzi@euris.it>
2025-03-16 23:45:39 +0100

    refactor CsvService and Home component to handle diff data; update Write method to accept a dictionary and adjust data writing logic

--- Lapo.Mud/Services/CsvService.cs
+++ Lapo.Mud/Services/CsvService.cs
@@ -18,23 +18,30 @@ public class CsvService
         _path = path;
     }
 
-    public void Write(List<DataRow> data)
+    public void Write(Dictionary<string, List<DataRow>> diff)
     {
-        if (data.Count == 0) return;
-
-        using var writer = new StreamWriter(_path, append: true);
-        using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
-
-        // Write header
-        foreach (DataColumn column in data.First().Table.Columns) csv.WriteField(column.ColumnName);
-        csv.NextRecord();
-
-        // Write rows
-        var rows = data.Select(row => row.ItemArray);
-        foreach (var row in rows)
+        foreach (var (key, data) in diff.OrderBy(x => x.Key))
         {
-            foreach (var item in row) csv.WriteField(item);
+            if (data.Count == 0) return;
+
+            using var writer = new StreamWriter(_path, append: true);
+            using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+            // Write table name
+            // csv.WriteField(key);
+            // csv.NextRecord();
+
+            // Write header
+            foreach (DataColumn column in data.First().Table.Columns) csv.WriteField(column.ColumnName);
             csv.NextRecord();
-        } 
+
+            // Write rows
+            var rows = data.Select(row => row.ItemArray);
+            foreach (var row in rows)
+            {
+                foreach (var item in row) csv.WriteField(item);
+                csv.NextRecord();
+            }
+        }
     }
 }
